64063f36ab3e11cfbcd62cfbf002900acc4de8ae
Pedro <pedrinhozenere99@gmail.com>
2020-09-03 22:39:11 -0400

    Finalizando estrutura de backend NodeJS com Typescript e frontend com NextJs com Typescript

--- frontend/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { AppProps } from 'next/app';
-
-const MyApp: React.FC = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -16,3 +16,5 @@ const Home: React.FC = () => {
     </div>
   );
 };
+
+export default Home;
